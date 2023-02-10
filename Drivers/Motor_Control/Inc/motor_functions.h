bf3e84b213464f1cd6fb696c89ff1e3031d1f495
Nathan Tang <nathantangca@outlook.com>
2023-02-10 02:20:42 -0500

    Added functionality for forward, backward, and stop motor control

--- Drivers/Motor_Control/Inc/motor_functions.h
+++ Drivers/Motor_Control/Inc/motor_functions.h
@@ -14,64 +14,82 @@
 #include <stdlib.h>
 #include "main.h"
 
+#define FCLK 84 // MHz
+#define PSC 0x0010 // prescaler 
+#define STEP_ANGLE 1.8F
+
 typedef enum {
-   FORWARD,
-   BACKWARD,
+  BACKWARD = 0,
+  FORWARD = 1,
+  NO_DIRECTION = 2,
 } motor_direction_t;
 
 /**
-  * @brief Struct to hold current status of motors
-  * @param direction direction of motor
-  * @param speed current speed of motor
+  * @brief Struct to hold configuration of a single motor
+  * @param directionPin microcontroller pin for motor driver DIR pin
+  * @param direction commanded direction for motor
   * @retval None
   */
 typedef struct {
-   motor_direction_t direction;
-   float speed;
+  uint16_t directionPin;
+  GPIO_TypeDef *directionPinPort;
+  motor_direction_t direction;
+} motor_config_t;
+
+/**
+  * @brief Struct to hold current status of motors
+  * @param timer PWM timer handle
+  * @param rightMotor configuration for right motor
+  * @param leftMotor configuration for left motor
+  * @param speed commanded speed of motors
+  * @param wheelRadius radius of wheel for motor to drive
+  */
+typedef struct {
+  TIM_HandleTypeDef *timer;
+  motor_config_t rightMotor;
+  motor_config_t leftMotor;
+  float speed;
+  float wheelRadius;
 } motor_instance_t;
  
 /**
   * @brief Move forwad at given speed
   * @param speed commanded speed to move motors forward
-  * @param rightMotor struct instance for right motor
-  * @param leftMotor struct instance for left motor
+  * @param motors struct instance of motors
   * @retval None
   */
-void MOTOR_move_speed_forward(float speed, motor_instance_t rightMotor, motor_instance_t leftMotor);
+void MOTOR_move_speed_forward(float speed, motor_instance_t motors);
 
 /**
   * @brief Move backward at given speed
   * @param speed commanded speed to move motors backward
-  * @param rightMotor struct instance for right motor
-  * @param leftMotor struct instance for left motor
+  * @param motors struct instance of motors
   * @retval None
   */
-void MOTOR_move_speed_backward(float speed, motor_instance_t rightMotor, motor_instance_t leftMotor);
+void MOTOR_move_speed_backward(float speed, motor_instance_t motors);
 
 /**
   * @brief Stop both motors
-  * @param None
+  * @param motors struct instance of motors
   * @retval None
   */
-void MOTOR_stop_both();
+void MOTOR_stop_both(motor_instance_t motors);
 
 /**
   * @brief Turn right
   * @param angle commanded angle to turn right
-  * @param rightMotor struct instance for right motor
-  * @param leftMotor struct instance for left motor
+  * @param motors struct instance of motors
   * @retval None
   */
-void MOTOR_turn_right(float angle, motor_instance_t rightMotor, motor_instance_t leftMotor);
+void MOTOR_turn_right(float angle, float speed, motor_instance_t motors);
 
 /**
   * @brief Turn left
   * @param angle commanded angle to turn left
-  * @param rightMotor struct instance for right motor
-  * @param leftMotor struct instance for left motor
+  * @param motors struct instance of motors
   * @retval None
   */
-void MOTOR_turn_left(float angle, motor_instance_t rightMotor, motor_instance_t leftMotor);
+void MOTOR_turn_left(float angle, float speed, motor_instance_t motors);
 
 
  #endif /* __MOTOR_CONTROL_H */
--- Drivers/Motor_Control/Src/motor_functions.c
+++ Drivers/Motor_Control/Src/motor_functions.c
@@ -6,23 +6,61 @@
   */
 
  #include "motor_functions.h"
+ #include "stm32f4xx_hal_tim.h"
 
-void MOTOR_move_speed_forward(float speed, motor_instance_t rightMotor, motor_instance_t leftMotor) {
+void MOTOR_move_speed_forward(float speed, motor_instance_t motors) {
+  float pwmFrequency = speed / (STEP_ANGLE * motors.wheelRadius);
+
+  uint32_t autoReloadRegister = FCLK / (pwmFrequency * (PSC + 1)) - 1;
+
+  MOTOR_stop_both(motors);
+
+  motors.rightMotor.direction = FORWARD;
+  motors.leftMotor.direction = FORWARD;
+
+  HAL_GPIO_WritePin(motors.rightMotor.directionPinPort, motors.rightMotor.directionPin, 1);
+  HAL_GPIO_WritePin(motors.rightMotor.directionPinPort, motors.rightMotor.directionPin, 0);
+
+  __HAL_TIM_SET_AUTORELOAD(motors.timer, autoReloadRegister);
+
+  motors.speed = speed;
+}
+
+void MOTOR_move_speed_backward(float speed, motor_instance_t motors) {
+  float pwmFrequency = speed / (STEP_ANGLE * motors.wheelRadius);
+
+  uint32_t autoReloadRegister = FCLK / (pwmFrequency * (PSC + 1)) - 1;
+
+  MOTOR_stop_both(motors);
+
+  motors.rightMotor.direction = BACKWARD;
+  motors.leftMotor.direction = BACKWARD;
+
+  HAL_GPIO_WritePin(motors.rightMotor.directionPinPort, motors.rightMotor.directionPin, 0);
+  HAL_GPIO_WritePin(motors.rightMotor.directionPinPort, motors.rightMotor.directionPin, 1);
+
+  __HAL_TIM_SET_AUTORELOAD(motors.timer, autoReloadRegister);
+
+  motors.speed = speed;
+}
+
+void MOTOR_stop_both(motor_instance_t motors) {
+  uint32_t autoReloadRegister = 0;
+  __HAL_TIM_SET_AUTORELOAD(motors.timer, autoReloadRegister);
+
+  motors.speed = 0;
+  motors.rightMotor.direction = NO_DIRECTION;
+  motors.leftMotor.direction = NO_DIRECTION;
+}
+
+void MOTOR_turn_right(float angle, float speed, motor_instance_t motors) {
+  
 
 }
 
-void MOTOR_move_speed_backward(float speed, motor_instance_t rightMotor, motor_instance_t leftMotor) {
-
-}
-
-void MOTOR_stop_both() {
-
-}
-
-void MOTOR_turn_right(float angle, motor_instance_t rightMotor, motor_instance_t leftMotor) {
-
-}
-
-void MOTOR_turn_left(float angle, motor_instance_t rightMotor, motor_instance_t leftMotor) {
+void MOTOR_turn_left(float angle, float speed, motor_instance_t motors) {
     
 }
+
+
+
