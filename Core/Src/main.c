f424d3eec1478de3a4a497bf7a81578b43073011
Nathan Tang <nathantangca@outlook.com>
2023-02-14 14:01:42 -0500

    Modified CRR to be nonzero

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -197,7 +197,7 @@ static void MX_TIM3_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
+  sConfigOC.Pulse = 100;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
