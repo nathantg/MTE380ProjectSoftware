d8f8b3f9091d7d033b8371448115a0359a8f1521
Nathan Tang <nathantangca@outlook.com>
2023-02-14 00:09:53 -0500

    Added configuration struct and a couple of function prototypes

--- Drivers/Ultrasonic/Inc/ultrasonic.h
+++ Drivers/Ultrasonic/Inc/ultrasonic.h
@@ -11,7 +11,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
+typedef struct {
+  uint16_t directionPin;
+  GPIO_TypeDef *directionPinPort;
+} ultrasonic_conig_t;
+
+void ULTRASONIC_init_config(ultrasonic_conig_t *ultrasonic, uint16_t directionPin, GPIO_TypeDef *directionPinPort);
+
+void ULTRASONIC_get_distance(float *distance, ultrasonic_conig_t *ultrasonic);
 
 
 #endif /* __ULTRASONIC_H */
--- Drivers/Ultrasonic/Src/ultrasonic.c
+++ Drivers/Ultrasonic/Src/ultrasonic.c
@@ -6,3 +6,11 @@
   */
 
  #include "ultrasonic.h"
+
+ void ULTRASONIC_init_config(ultrasonic_conig_t *ultrasonic, uint16_t directionPin, GPIO_TypeDef *directionPinPort) {
+
+ }
+
+ void ULTRASONIC_get_distance(float *distance, ultrasonic_conig_t *ultrasonic) {
+
+ }
