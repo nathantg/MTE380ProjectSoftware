7025e8c70fa3a60e2f7c24c2aea2435d97e9b9cd
Nathan Tang <nathantangca@outlook.com>
2023-02-14 14:12:50 -0500

    Added motor function header file to main

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "motor_functions.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
