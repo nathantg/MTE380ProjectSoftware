448c5d50186e56cb6a9e9be877bec777b88eef72
Nathan Tang <nathantangca@outlook.com>
2023-02-14 17:23:26 -0500

    Included ultrasonic header in main

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include  "ultrasonic.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
