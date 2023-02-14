cd26a4b2b14520edee644f767da6ceb446989334
Nathan Tang <nathantangca@outlook.com>
2023-02-14 02:54:09 -0500

    Added functionality for init and get_distance functions

--- Drivers/Ultrasonic/Inc/ultrasonic.h
+++ Drivers/Ultrasonic/Inc/ultrasonic.h
@@ -16,11 +16,18 @@
 typedef struct {
   uint16_t directionPin;
   GPIO_TypeDef *directionPinPort;
+  uint16_t trigPin;
+  GPIO_TypeDef *trigPinPort;
+  uint16_t echoPin;
+  GPIO_TypeDef *echoPinPort;
+
 } ultrasonic_conig_t;
 
-void ULTRASONIC_init_config(ultrasonic_conig_t *ultrasonic, uint16_t directionPin, GPIO_TypeDef *directionPinPort);
+void ULTRASONIC_init_config(ultrasonic_conig_t *ultrasonic, uint16_t directionPin, GPIO_TypeDef *directionPinPort, 
+                                                            uint16_t trigPin, GPIO_TypeDef *trigPinPort,
+                                                            uint16_t echoPin, GPIO_TypeDef *echoPinPort);
 
-void ULTRASONIC_get_distance(float *distance, ultrasonic_conig_t *ultrasonic);
+void ULTRASONIC_get_distance(uint32_t *distance, ultrasonic_conig_t *ultrasonic);
 
 
 #endif /* __ULTRASONIC_H */
--- Drivers/Ultrasonic/Src/ultrasonic.c
+++ Drivers/Ultrasonic/Src/ultrasonic.c
@@ -7,10 +7,24 @@
 
  #include "ultrasonic.h"
 
- void ULTRASONIC_init_config(ultrasonic_conig_t *ultrasonic, uint16_t directionPin, GPIO_TypeDef *directionPinPort) {
-
+ void ULTRASONIC_init_config(ultrasonic_conig_t *ultrasonic, uint16_t directionPin, GPIO_TypeDef *directionPinPort, 
+                                                            uint16_t trigPin, GPIO_TypeDef *trigPinPort,
+                                                            uint16_t echoPin, GPIO_TypeDef *echoPinPort) {
+  ultrasonic->directionPinPort = directionPinPort;
+  ultrasonic->directionPin = directionPin;
+  ultrasonic->trigPin = trigPin;
+  ultrasonic->trigPinPort = trigPinPort;
+  ultrasonic->echoPin = echoPin;
+  ultrasonic->echoPinPort = echoPinPort;
  }
 
- void ULTRASONIC_get_distance(float *distance, ultrasonic_conig_t *ultrasonic) {
+ void ULTRASONIC_get_distance(uint32_t *distance, ultrasonic_conig_t *ultrasonic) {
+  HAL_GPIO_WritePin(ultrasonic->trigPinPort, ultrasonic->trigPin, 1);
+  HAL_Delay(1); // this delay is too long. Change later
+  uint32_t t1 = HAL_GetTick();
+  while(HAL_GPIO_ReadPin(ultrasonic->echoPinPort, ultrasonic->echoPin) == 0) {}
+  uint32_t t2 = HAL_GetTick();
+  uint32_t pulseWidth = t2 - t1;
 
+  *distance = pulseWidth / 58; // distance in cm
  }
