96e1f9498eebdfb197bb279b4ed55f4d6cbe3e97
Nathan Tang <nathantangca@outlook.com>
2023-02-14 22:02:37 -0500

    Configured trig and echo pins for ultrasonic sensor

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -71,6 +71,10 @@ void Error_Handler(void);
 #define TCK_GPIO_Port GPIOA
 #define SWO_Pin GPIO_PIN_3
 #define SWO_GPIO_Port GPIOB
+#define Ultrasonic_Trig_Pin GPIO_PIN_5
+#define Ultrasonic_Trig_GPIO_Port GPIOB
+#define Ultrasonic_Echo_Pin GPIO_PIN_6
+#define Ultrasonic_Echo_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
