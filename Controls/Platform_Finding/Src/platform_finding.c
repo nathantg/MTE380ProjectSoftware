802001a1e87f5861b81315942d84b08dda29d4b5
Nathan Tang <nathantangca@outlook.com>
2023-03-16 15:39:45 -0400

    Removed merged stuff from controls

--- Controls/Platform_Finding/Src/platform_finding.c
+++
@@ -1,8 +0,0 @@
-/**
-  ******************************************************************************
-  * @file           : platform_finding.c
-  * @brief          : functions for platform finding algorithm
-  ******************************************************************************
-*/
-
-#include "platform_finding.h"
