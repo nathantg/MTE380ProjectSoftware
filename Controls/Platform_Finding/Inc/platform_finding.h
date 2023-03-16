d8dfccf2776e4cdc673f9e5994c9254515fafe06
Nathan Tang <nathantangca@outlook.com>
2023-03-16 01:19:42 -0400

    Skelaton for platform finding algorithm

---
+++ Controls/Platform_Finding/Inc/platform_finding.h
@@ -0,0 +1,19 @@
+/**
+  ******************************************************************************
+  * @file           : platform_finding.h
+  * @brief          : Header for platform_finding.c file.
+  *                   This file contains the functions prototypes for the platform finding
+  *                   algorithm
+  ******************************************************************************
+*/
+
+#ifndef __PLATFORM_FINDING_H
+#define __PLATFORM_FINDING_H
+
+#include "main.h"
+#include "motor_functions.h"
+#include "switches.h"
+#include "MB1040.h"
+
+
+#endif /* __PLATFORM_FINDING_H */
---
+++ Controls/Platform_Finding/Src/platform_finding.c
@@ -0,0 +1,8 @@
+/**
+  ******************************************************************************
+  * @file           : platform_finding.c
+  * @brief          : functions for platform finding algorithm
+  ******************************************************************************
+*/
+
+#include "platform_finding.h"
