c92e8a5a2aaca1477453600a5ac51a81ec109072
Nathan Tang <nathantangca@outlook.com>
2023-03-22 02:47:47 -0400

    Modified to log both up and down tilt switches

--- Drivers/Logging/Src/logging.c
+++ Drivers/Logging/Src/logging.c
@@ -9,16 +9,16 @@
  #include <stdio.h>
  #include <string.h>
 
- void logging(UART_HandleTypeDef *huart, float distance, uint8_t limitSwitchValue, uint8_t tiltSwitchValue, motor_instance_t *motorInstance) {
+ void logging(UART_HandleTypeDef *huart, float distance, switches_instance_t *switchesInstance, motor_instance_t *motorInstance) {
     uint8_t buf[100];
 
     distance *= 100;
     sprintf((char*)buf, 
-            "Distance: %u.%u in, Limit Switch: %d, Tilt Switch: %d, PWM ARR: %d\r\n",
+            "Distance: %u.%u in, Up Tilt Switch: %d, Down Tilt Switch: %d, PWM ARR: %d\r\n",
             ((unsigned int)distance / 100),
             ((unsigned int)distance % 100), 
-            limitSwitchValue, 
-            tiltSwitchValue, 
+            switchesInstance->upTiltSwitch->switchStatus,
+            switchesInstance->downTiltSwitch->switchStatus, 
             (int)motorInstance->autoReloadRegister);
 
     HAL_UART_Transmit(huart, buf, strlen((char*)buf), HAL_MAX_DELAY);
