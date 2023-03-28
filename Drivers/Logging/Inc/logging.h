cf242e7b956c940028ddb809687635e1cff1c6e2
Nathan Tang <nathantangca@outlook.com>
2023-03-27 23:41:54 -0400

    Added logging of gyro pitch and roll

--- Drivers/Logging/Inc/logging.h
+++ Drivers/Logging/Inc/logging.h
@@ -14,6 +14,7 @@
 #include "motor_functions.h"
 #include "switches.h"
 #include "MB1040.h"
+#include "mpu6050.h"
 
 /**
  * @brief Initialize limit and tilt switch configuration
@@ -21,8 +22,9 @@
  * @param distance variable with distance value
  * @param switchesInstance pointer to switches instance struct
  * @param motorInstance pointer to motor instance struct
+ * @param gyroInstance pointer to gyro instance struct
  * @retval None
  */
-void logging(UART_HandleTypeDef *huart, float distance, switches_instance_t *switchesInstance, motor_instance_t *motorInstance);
+void logging(UART_HandleTypeDef *huart, float distance, switches_instance_t *switchesInstance, motor_instance_t *motorInstance, MPU6050_t *gyroInstance);
 
 #endif /* __LOGGING_H */
